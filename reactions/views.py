31ba8caec80bd0c9049e000335478ee1828f9d2a
Alessandro Rossi <rossisverige@yahoo.se>
2023-11-30 14:00:21 +0000

    Resolved queryset reactions

--- reactions/views.py
+++ reactions/views.py
@@ -2,15 +2,16 @@ from rest_framework import generics, permissions
 from pp_api.permissions import IsOwnerOrReadOnly
 from reactions.models import Reaction
 from reactions.serializers import ReactionSerializer
+from django.db.models import Count
 
 
 class ReactionList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = ReactionSerializer
     queryset = Reaction.objects.annotate(
-        reactions_count_hydrate=Count('reaction_type=1'),
-        reactions_count_moist=Count('reaction_type=2'),
-        reactions_count_dry=Count('reaction_type=3'),
+        reactions_count_hydrate=Count('reaction_type', filter=reaction_type=1),
+        reactions_count_moist=Count('reaction_type', filter=reaction_type=2),
+        reactions_count_dry=Count('reaction_type', filter=reaction_type=3),
     )
 
     def perform_create(self, serializer):
