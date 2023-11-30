d63189afcb3a7eb6d63a00dfbaba95e8e73bf21d
Alessandro Rossi <rossisverige@yahoo.se>
2023-11-30 15:46:30 +0000

    Debug = true

--- pp_api/settings.py
+++ pp_api/settings.py
@@ -59,7 +59,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alessandrorossi8-ppapi-ogvveuooiuv.ws-eu106.gitpod.io', 'plant-parenthood-pp5-ac00fe42de7c.herokuapp.com']
 
