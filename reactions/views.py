a0de88968f0f3d48291a47b557ab5936d1573ef1
Alessandro Rossi <rossisverige@yahoo.se>
2023-11-30 14:08:11 +0000

    Solved queryset reaction count

--- reactions/views.py
+++ reactions/views.py
@@ -8,11 +8,7 @@ from django.db.models import Count
 class ReactionList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = ReactionSerializer
-    queryset = Reaction.objects.annotate(
-        reactions_count_hydrate=Count('reaction_type', filter=reaction_type=1),
-        reactions_count_moist=Count('reaction_type', filter=reaction_type=2),
-        reactions_count_dry=Count('reaction_type', filter=reaction_type=3),
-    )
+    queryset = Reaction.objects.values('reaction_type').annotate(reaction_count=Count('reaction_type'))
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
