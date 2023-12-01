e5abbdc802d1c342598b43a83ad1385d8f5b5f56
Alessandro Rossi <rossisverige@yahoo.se>
2023-12-01 15:22:04 +0000

    added reactions_id

--- reactions/serializers.py
+++ reactions/serializers.py
@@ -8,6 +8,10 @@ class ReactionSerializer(serializers.ModelSerializer):
     reactions_count_moist = serializers.ReadOnlyField()
     reactions_count_dry = serializers.ReadOnlyField()
 
+    reactions_id = serializers.ReadOnlyField(source='id')
+
     class Meta:
         model = Reaction
-        fields = ['__all__']
+        fields = ['id', 'created_at', 'owner', 'plant', 'reaction_type', 
+        'reactions_count_hydrate', 'reactions_count_moist', 
+        'reactions_count_dry', 'reactions_id']
