6047cf428eedf0434116da99f06e549fe33ae933
Alessandro Rossi <rossisverige@yahoo.se>
2023-12-04 14:29:44 +0000

    Migrated changes to plant_requests

--- plant_requests/urls.py
+++ plant_requests/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import NewPlantRequest, ApprovePlantRequest, DenyPlantRequest,
+from .views import NewPlantRequest, ApprovePlantRequest, DenyPlantRequest
 
 
 urlpatterns = [
