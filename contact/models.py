4711671b8ca7ac0dc906a9a20bc15464257d3c05
Alessandro Rossi <rossisverige@yahoo.se>
2023-12-04 13:24:48 +0000

    Changed message field to content in contact model

--- contact/models.py
+++ contact/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Contact(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=255)
-    message = models.TextField()
+    content = models.TextField()
     link = models.TextField()
 
     def __str__(self):
--- contact/serializers.py
+++ contact/serializers.py
@@ -4,9 +4,9 @@ from .models import Contact
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
-        fields = ['id', 'owner', 'subject', 'message', 'link']
+        fields = ['id', 'owner', 'subject', 'content', 'link']
 
 class ContactFormSerializer(serializers.Serializer):
     subject = serializers.CharField(max_length=255)
-    message = serializers.CharField()
+    content = serializers.CharField()
     link = serializers.CharField()
